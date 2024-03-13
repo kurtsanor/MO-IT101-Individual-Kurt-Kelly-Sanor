07d73c2c094b8409ccdd96aeee6c2b7e3efbb7dc
keith <keith@LAPTOP-59IM73VJ>
2024-03-13 12:16:39 +0800

    commit

--- src/kurt/InitialCode.java
+++ src/kurt/InitialCode.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 public class InitialCode {
 
 	public static void main(String[] args) {
